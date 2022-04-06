8b5f6ef7cadc211c5c92b24400243e5a31126ade
yusuf <yusuffront@gmail.com>
2022-04-06 10:39:47 +0800

    next on tailwinds

---
+++ tailwind1/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class',
+  theme: {
+      extend: {
+          spacing: {
+              '13': '3.25rem',
+          },
+          fontFamily: {
+              inter: ['Inter'],
+          },
+          colors : {
+              hero: 'green'
+          },
+          animation: {
+              'spin-slow': 'spin 3s linear infinite',
+              wiggle: 'wiggle 1s ease-in-out infinite',
+          },
+          keyframes: {
+              wiggle: {
+              '0%, 100%': { transform: 'rotate(-3deg)' },
+              '50%': { transform: 'rotate(3deg)' },
+              }
+          }
+      }
+    },
+  plugins: [],
+}
